5fa808acc61af6643f602abb9f583d654958710a
Varun Colbert <c_vcolbe@quicinc.com>
2009-10-21 15:55:32 -0700

    omx-mm-video: Use relative path to call the makefiles

--- Android.mk
+++ Android.mk
@@ -25,7 +25,11 @@
 #OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 #ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #--------------------------------------------------------------------------
+
+OMX_VIDEO_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
 ifdef BOARD_USES_QCOM_8x_CHIPSET
-    include vendor/qcom-opensource/omx/mm-video/8k/vdec/Android.mk
+    include $(OMX_VIDEO_PATH)/8k/vdec/Android.mk
 endif
 
